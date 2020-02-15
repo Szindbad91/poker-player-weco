6ea6654a8b4fdb3118f23b8caff10f311372adff
vaszka <vaszka_misi@yahoo.com>
2020-02-15 13:19:06 +0100

    cards

--- CardCombinationHandler.php
+++ CardCombinationHandler.php
@@ -3,6 +3,7 @@
 
 class CardCombinationHandler
 {
+   protected $cards;
     var $c1, $c2, $c3, $c4, $c5, $c6, $c7;
 
     /**
@@ -17,13 +18,7 @@ class CardCombinationHandler
      */
     public function __construct($c1, $c2, $c3 = null, $c4 = null, $c5 = null, $c6 = null, $c7 = null)
     {
-        $this->c1 = $c1;
-        $this->c2 = $c2;
-        $this->c3 = $c3;
-        $this->c4 = $c4;
-        $this->c5 = $c5;
-        $this->c6 = $c6;
-        $this->c7 = $c7;
+        $this->cards=func_get_args(); 
     }
 
     public function getCombination()
